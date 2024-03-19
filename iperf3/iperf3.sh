b9b0ab2085241b803f7d709eeec7aa6c3e6c7b28
Shakiba Moshiri <net@shakiba.net>
2024-03-19 18:07:39 +0330

    Update iperf3

--- iperf3/iperf3.sh
+++ iperf3/iperf3.sh
@@ -6,8 +6,10 @@ apt-get update -y
 apt-get install -y build-essential
 
 ### download 
-git clone https://github.com/esnet/iperf.git
+git clone https://github.com/esnet/iperf.git || true
 cd iperf
+git config pull.rebase true
+git pull
 
 ### build
 ./configure
